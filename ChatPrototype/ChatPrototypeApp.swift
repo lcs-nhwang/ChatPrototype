a1de69d26c04bd5cfe6c6f55cdb10aafc382bb88
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-03 08:43:13 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Nicholas Hwang on 3/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
